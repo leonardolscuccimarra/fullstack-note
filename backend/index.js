9ca8b608347fa1f487dea0f00c6d5a6469f3931a
leonardolscuccimarra <leonardolscuccimarra@gmail.com>
2026-03-15 19:03:33 -0300

    add CORS to backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from "cors";
 import {sequelize, connectDatabase} from './src/config/database.js';
 import docsRoutes from './src/routes/docs.routes.js';
 import noteRoutes from './src/routes/note.routes.js';
 import categoryRoutes from './src/routes/category.routes.js';
- import { setupAssociations } from './src/models/associations.js';
+import { setupAssociations } from './src/models/associations.js';
+
+
 
 dotenv.config();
 
@@ -12,8 +15,16 @@ const app = express();
 const URL = process.env.URL ?? "http://localhost";
 const PORT = process.env.PORT ?? 8080;
 
+const FRONT_URL = process.env.FRONT_URL ?? "http://localhost"
+const FRONT_PORT = process.env.FRONT_PORT ?? 3000
+
 
 app.use(express.json());
+
+app.use(cors({
+  origin: `${FRONT_URL}:${FRONT_PORT}`
+}));
+
 app.use('', docsRoutes);
 app.use('/api', docsRoutes);
 app.use('/api/notes', noteRoutes);
